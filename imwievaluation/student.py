dee298547c76cdce3fff64c31299e06ea0c757fd
ESchae <ellifant_smile@web.de>
2018-06-19 23:20:05 +0200

    add alternate constructor get_unique for class Student

--- imwievaluation/student.py
+++ imwievaluation/student.py
@@ -6,9 +6,9 @@ class Student(Base):
     __tablename__ = 'student'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30))
-    last_name = db.Column(db.String(30))
-    email = db.Column(db.String(30))
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
 
     # prevent duplicate entries
     db.UniqueConstraint(first_name, last_name, email)
@@ -17,3 +17,30 @@ class Student(Base):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+
+    @classmethod
+    def get_unique(cls, first_name, last_name, email, session):
+        """ Alternate constructor to get a unique instance of the class.
+
+        Checks a cache of existing uncommited instances or
+        queries the database for existing commited instance
+        before returning a new instance.
+
+        Taken with modifications from:
+        https://stackoverflow.com/questions/12787452/
+
+        """
+        # get the session cache, creating it if necessary
+        cache = session._unique_cache = getattr(session, '_unique_cache', {})
+        instance_key = (cls, first_name, last_name, email)
+        instance = cache.get(instance_key)  # check if it exists in the cache
+        if instance is None:  # check if it exists in the database
+            instance = session.query(cls)\
+                .filter_by(first_name, last_name, email)\
+                .first()
+            if instance is None:  # create a new instance
+                instance = cls(first_name, last_name, email)
+                session.add(instance)
+            # update the cache
+            cache[instance_key] = instance
+        return instance
--- imwievaluation/tests/test_student.py
+++ imwievaluation/tests/test_student.py
@@ -21,3 +21,27 @@ class TestStudent(DatabaseTest):
 
         result = self.session.query(Student).count()
         self.assertEqual(2, result)
+
+    def test_get_unique_existing_in_cache(self):
+        """ See https://stackoverflow.com/questions/12787452/ """
+        num_students_before = self.session.query(Student).count()
+        s1 = Student.get_unique(  # new
+            'Max', 'Mustermann', 'max@mustermann.de', self.session)
+        s2 = Student.get_unique(  # taken from cache
+            'Max', 'Mustermann', 'max@mustermann.de', self.session)
+        num_students_after = self.session.query(Student).count()
+
+        self.assertEqual(num_students_after, num_students_before + 1)
+
+    def test_get_unique_existing_in_database(self):
+        """ See https://stackoverflow.com/questions/12787452/ """
+        num_students_before = self.session.query(Student).count()
+        s1 = Student.get_unique(  # new
+            'Max', 'Mustermann', 'max@mustermann.de', self.session)
+        self.session.commit()  # s1 is now in database
+        self.session._unique_cache.clear()  # cache is now empty
+        s2 = Student.get_unique(  # taken from database
+            'Max', 'Mustermann', 'max@mustermann.de', self.session)
+        num_students_after = self.session.query(Student).count()
+
+        self.assertEqual(num_students_after, num_students_before + 1)
